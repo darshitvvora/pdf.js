90db92560bc544a10c37a0653ca236be2154c94c
Jonas Jenwald <jonas.jenwald@gmail.com>
2016-09-01 13:23:18 +0200

    Change the `GoToPage` Named Action to select the contents of the `pageNumber` input, rather than just focusing the element
    When clicking on the `pageNumber` input, or when using the keyboard shortcut <kbd>Ctrl</kbd>+<kbd>Alt</kbd>+<kbd>G</kbd>, the element isn't just focused but its contents is actually *selected* so that the user doesn't need to clear the input before entering a new `pageNumber`.
    However, the `GoToPage` named action is only using `focus`, so let's change this to be consistent.
    
    The following document can be used for testing: http://www2.informatik.uni-freiburg.de/~frank/ENG/beamer/example/Beamer-class-example1.pdf#zoom=page-fit
    
    [screeenshot]

--- web/app.js
+++ web/app.js
@@ -1709,7 +1709,7 @@ function webViewerNamedAction(e) {
   var action = e.action;
   switch (action) {
     case 'GoToPage':
-      PDFViewerApplication.appConfig.toolbar.pageNumber.focus();
+      PDFViewerApplication.appConfig.toolbar.pageNumber.select();
       break;
 
     case 'Find':
